6bedfb6dc38ac0d7b5e28ad05b267cfaa2b65d7a
cameron <cameron-04@hotmail.com>
2024-09-24 10:47:09 -0500

    fix uneven homepage showcase sections

--- app/ui/home/options_showcase.tsx
+++ app/ui/home/options_showcase.tsx
@@ -4,7 +4,7 @@ import CustomizeAnim from "./customize_anim";
 
 export default function OptionsShowcase() {
     return <div className="flex bg-camsii-offwhite p-[10px] justify-between">
-        <div className="flex-grow flex flex-col items-center justify-between p-[30px] pt-[70px] bg-gradient-to-b from-[#D8A0B1] to-[#986373] h-[625px]">
+        <div className="flex-1 flex flex-col items-center justify-between p-[30px] pt-[70px] bg-gradient-to-b from-[#D8A0B1] to-[#986373] h-[625px]">
             <p className="text-black text-[40px]">Pick from our curated selection</p>
             <></>
             <Button
@@ -17,7 +17,7 @@ export default function OptionsShowcase() {
                 style={{paddingLeft: 50, paddingRight: 50, paddingTop: 25, paddingBottom: 25, borderRadius: 20}}
             />
         </div>
-        <div className="relative flex-grow ml-[10px] flex flex-col justify-between items-center p-[30px] pt-[110px] bg-gradient-to-b from-[#222222] to-[#111111]">
+        <div className="relative flex-1 ml-[10px] flex flex-col justify-between items-center p-[30px] pt-[110px] bg-gradient-to-b from-[#222222] to-[#111111]">
             <p className="absolute left-[30px] top-[30px] text-[30px] text-camsii-gray">or...</p>
             <p className="text-[40px]">Personalize your order</p>
             <CustomizeAnim />
