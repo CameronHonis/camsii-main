82168fc0a02903ec560c2cb20c7b8d3ebcef92cd
cameron <cameron-04@hotmail.com>
2024-09-25 17:51:57 -0500

    fix swapped order button links

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -8,7 +8,7 @@ export default function Order() {
             <div className="flex flex-col items-center max-w-[420px] mx-[75px]">
                 <p className="text-black text-[25px] text-center">Choose from a curated list of popular words and phrases</p>
                 <NavButton
-                    href="/order/custom"
+                    href="/order/curated"
                     contents={<>
                         Go Tried and True
                         <DiscountTag discountPerc={15} />
@@ -21,7 +21,7 @@ export default function Order() {
             <div className="flex flex-col items-center max-w-[420px] mx-[75px]">
                 <p className="text-black text-[25px] text-center">Customize the size and shape of the letters you want, how you want</p>
                 <NavButton
-                    href="/order/curated"
+                    href="/order/custom"
                     contents={<>
                         Get Customizing
                     </>}
